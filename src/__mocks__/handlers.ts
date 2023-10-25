9581a661626bbe67f063bde2ea4031563548193e
J <jacob@lapworth.nz>
2023-10-25 14:39:53 +1300

    refactor urls

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -7,26 +7,26 @@ import {
 import { reactions } from './fixtures/comments.js'
 import { activityLogsResponse } from './fixtures/activityLogs.js'
 
+const BASE_URL = 'https://api.figma.com/v1'
+
 export const handlers = [
-  // v1/files/${fileKey}/variables/local
-  http.get('https://api.figma.com/v1/files/:key/variables/local', () =>
+  http.get(`${BASE_URL}/files/:key/variables/local`, () =>
     HttpResponse.json(localVariablesResponse)
   ),
 
-  http.get('https://api.figma.com/v1/files/:key/variables/published', () =>
+  http.get(`${BASE_URL}/files/:key/variables/published`, () =>
     HttpResponse.json(publishedVariablesResponse)
   ),
 
-  http.post('https://api.figma.com/v1/files/:key/variables', () =>
+  http.post(`${BASE_URL}/files/:key/variables`, () =>
     HttpResponse.json(modifyVariablesResponse)
   ),
 
-  http.get(
-    'https://api.figma.com/v1/files/:file/comments/:comment/reactions',
-    () => HttpResponse.json({ reactions })
+  http.get(`${BASE_URL}/files/:file/comments/:comment/reactions`, () =>
+    HttpResponse.json({ reactions })
   ),
 
-  http.get('https://api.figma.com/v1/activity_logs', () =>
+  http.get(`${BASE_URL}/activity_logs`, () =>
     HttpResponse.json(activityLogsResponse)
   ),
 ]
