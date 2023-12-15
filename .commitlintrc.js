3377f2abb463030fc3005a7c5141549f9e68dab8
J <jacob@lapworth.nz>
2023-12-15 14:41:01 +1300

    ci: allow infinite footer commit message length (#87)

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -2,5 +2,6 @@ export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-max-length': [0, 'always', Infinity],
+    'footer-max-length': [0, 'always', Infinity],
   },
 }
